dc8b51f4dfc8848dcbaa00e67a45dc948027e8f5
sergm2324 <sergm2324@gmail.com>
2019-05-30 23:19:18 +0300

    1. Используя Автогенератор создал CRUD для таблиц Tasks и Users. 2. На стартовой странице с помощью Widget ListView вывел список задач, при этом каждая карточка выведена с помощью моего виджета CardWidget. 3. При нажатии на карточку-превью сделал переход по ссылке на форму редактирования данной карточки.

--- widgets/views/card.php
+++ widgets/views/card.php
@@ -39,25 +39,27 @@
     }
 </style>
 
-
-<div class="border">
-    <div class="wrap">
-        <div class="product-wrap">
-            <h4 class="card-title"><?=$model['description']?></h4>
+<a href="/index.php?r=tasks/update&id=<?=$model['id']?>">
+    <div class="border">
+        <div class="wrap">
+            <div class="product-wrap">
+                <h4 class="card-title"><?=$model['description']?></h4>
+            </div>
+        </div>
+        <div class="product-info">
+            <div class="stars"></div>
+            <h3 class="product-title">Создатель:</h3>
+            <div class="product"><?=$model->usercr->username?></div>
+            <h3 class="product-title">Ответственный:</h3>
+            <div class="product"><?=$model->userres->username?></div>
+            <h3 class="product-title">Срок выполнения:</h3>
+            <div class="product"><?=$model['deadline']?></div>
+            <h3 class="product-title">Текущий статус:</h3>
+            <div class="product"><?=$model->status->name?></div>
         </div>
     </div>
-    <div class="product-info">
-        <div class="stars"></div>
-        <h3 class="product-title">Создатель:</h3>
-        <div class="product"><?=$model->usercr->username?></div>
-        <h3 class="product-title">Ответственный:</h3>
-        <div class="product"><?=$model->userres->username?></div>
-        <h3 class="product-title">Срок выполнения:</h3>
-        <div class="product"><?=$model['deadline']?></div>
-        <h3 class="product-title">Текущий статус:</h3>
-        <div class="product"><?=$model->status->name?></div>
-    </div>
-</div>
+</a>
+
 
 
 
